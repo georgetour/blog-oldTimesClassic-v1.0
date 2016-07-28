c6f337a7413ec9fc54b282afe71576621eb2ad6f
George Tourtsinakis <tourtsinakis@outlook.com>
2016-07-28 21:12:22 +0300

    Almost finished suggest a game page
    Needs some minor tweaking, captcha and back end.Looks nice!

--- suggestagame.php
+++ suggestagame.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Sports games</title>
+    <title>Suggest a game</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -14,7 +14,7 @@
     <script async src="scripts/index.js"></script>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-    <link href="stylesheets/recentStylesheet.css" type="text/css" rel="stylesheet">
+    <link href="stylesheets/suggestAgameStylesheet.css" type="text/css" rel="stylesheet">
 
 
 </head>
@@ -65,60 +65,54 @@
 </nav><!--End top menu bar-->
 
 <div class="container"><!--Main container below the navbar-->
-    <h2 class="mainHeader">Old times classic sport games.</h2>
-    <p class="genreDescirption">Sport games don't need description.Basketball,football,soccer and whatever else that requires
-        physical ability and skills is considered that.
+    <h2 class="mainHeader">This is <strong>YOUR</strong> SECTION! Suggest a game !</h2>
+    <p class="genreDescirption">
+        We have heard many games that we didn't know from friends and others so if you think
+        a game must be an old Times Classic bring it on.
     </p>
+    <div class="row ">
+        <form class=" col-lg-12 col-md-12 col-xs-12  col-sm-12" role="form">
+            <div class="  col-lg-6 col-md-6 col-md-12 col-xs-12 ">
+
+                <div class="formRow ">
+                <label  class=" ">Game Title : </label>
+                <input class="formField"  placeholder="ex Pac-man" type="text">
+                </div>
+
+                <div class=" formRow">
+                    <label  class="">Release Date : </label>
+                <input class="formField"  placeholder="ex 1995" type="text">
+                </div>
+                <div class=" formRow ">
+                    <label  class="">Type : </label>
+                    <input class="formField"  placeholder="ex Sports" type="text">
+                </div>
+                <div class=" formRow ">
+                    <label  class=" descriptionLabel">Suggested By : </label>
+                    <input class="formField "  placeholder="full name or nickname" type="text">
 
 
-    <div class="imageRow col-lg-12 col-md-12 col-xs-12  col-sm-12">
-        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-            <img class="  imagesMaxheight img-responsive center-block" src="images/gameCovers/tekhan_world_cup.gif">
-        </div>
+                </div>
 
-        <aside class=" col-lg-6 col-md-6 col-sm-12 col-xs-12 testBorder">
+                <div class=" formRow  ">
+                    <label  class="descriptionLabel">Email : </label>
+                    <input class="formField"  placeholder="News and your game" type="text">
+                </div>
 
-            <h3 class="descriptionHeader">
-                Tehkan World Cup
-            </h3>
-            <p>Release Date:  1985</p>
-            <p>Type :Sports</p>
-            <p>Suggested by : George Tourtsinakis</p>
-            <p>Description:</p>
+            </div>
+            <div class="col-lg-6 col-md-6 col-md-12 col-xs-12">
+                <div class="   descriptionLabel">
+                    <label  class=" ">Description :</label>
+                    <textarea class="formField formDescription"  placeholder="Be short like our descriptions" type="text" rows="5"></textarea>
 
-            <p>Oh man this game took a lot of my coins .  </p>
-            <p> Fantastic two player or single player game that evolved through years and gave us amazing sequels.  </p>
-            <p>I miss games like Tehkan world cup.</p>
-            <p>Of course it has a place as an old Times Classic. </p>
+                </div>
+                <div>Captcha</div>
+                <div><input class="submitButton" type="submit" value="Suggest a game!"></div>
+            </div>
 
-
-        </aside>
-    </div>
-
-    <div class="imageRow col-lg-12 col-md-12 col-xs-12  col-sm-12">
-        <div class="col-lg-6 col-md-6 col-xs-12  col-sm-12  ">
-            <img class=" imagesMaxheight img-responsive center-block  "  src="images/gameCovers/Nba-live-95.jpg">
-        </div>
-
-        <aside class="col-lg-6 col-md-6 col-xs-12  col-sm-12 testBorder">
-
-            <h3 class="descriptionHeader  ">
-                NBA live 95
-            </h3>
-            <p>Release Date: 1994</p>
-            <p>Type : Sports</p>
-            <p>Suggested by : George Toutsinakis</p>
-            <p>Description:</p>
-
-            <p>NBA live 95 had really unique and stylish gameplay . </p>
-            <p>I remember seeing small tournaments and a lot of people playing it.  </p>
-            <p> Even if you don;t like basketball you can try this title to see what we mean by gameplay. </p>
-            <p>Unforgettable old Times Classic game.   </p>
-
-        </aside>
+        </form>
 
     </div>
-
 
 
 </div>
